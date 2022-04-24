395b36776318e87e130febf706126cbff7b499cf
vkhramov <khramov.vladimir@gmail.com>
2022-04-24 16:33:30 +0300

    upd

--- parserweb/parser.py
+++ parserweb/parser.py
@@ -1,4 +1,6 @@
+import chunk
 from pprint import pprint
+from urllib import response
 import requests
 from bs4 import BeautifulSoup
 from os import sys
@@ -7,6 +9,8 @@ import json
 from progress.bar import IncrementalBar
 from parserweb.config import BASE_URL
 from db.model import db, Rsodata
+import asyncio
+import aiohttp
 
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.174 YaBrowser/22.1.3.856 (beta) Yowser/2.5 Safari/537.36"
@@ -37,30 +41,58 @@ def get_data(source_type):
     if source_type == 'file':
         return get_data_from_file('registry.html')
 
+
+async def get_url(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
+
+
+async def get_data_from_url_list(url_list, chunk=50, pause=1):
+    list_len = len(url_list)
+    bar = IncrementalBar('Getting user info', max = list_len)
+    start_range = 0
+    end_range = chunk
+    fetched_data_dict = {}
+    while start_range != list_len:
+        for idx in range(start_range, end_range):
+            data = await get_url(url_list[idx])
+            fetched_data_dict[url_list[idx]] = data
+        bar.next(n=end_range-start_range)
+        await asyncio.sleep(pause)
+        start_range = end_range
+        end_range = (end_range + chunk) if (end_range + chunk <= list_len) else list_len
+    bar.finish()
+    return fetched_data_dict
+
 def parse_main_page():
     content = get_data('url')
     soup = BeautifulSoup(content, "html.parser")
     all_rows = soup.find_all('td', class_="left-td")
+    # print(all_rows)
     # all_rows = table.find_all('tr')
     person_list = []
     all_rows_len = len(all_rows)
-    bar = IncrementalBar('Getting user info', max = all_rows_len)
-
-    for index, row in enumerate(all_rows):
+    
+    url_list = []
+    for row in all_rows:
         href_obj = row.find('a')
         url = BASE_URL+href_obj['href']
-        bar.next()
+        url_list.append(url)
+    
+    all_data = asyncio.run(get_data_from_url_list(url_list))
+    
+    for url, user_data in all_data.items():
         # print(f"Getting data for persons {index+1}, remaining {all_rows_len-index-1} persons to get")
-        user_data = get_user_info(url, headers=headers)
         user_parsed_data = parse_user_info(user_data, url)
         error_list = []
         if not user_parsed_data['lfm']:
-            error_list.append((href_obj.text, url))
+            error_list.append(url)
             continue
         person_list.append(user_parsed_data)
-    bar.finish()
+    
     for error in error_list:
-        print(f"Can't get user info for {error[0]} from url {error[1]}")
+        print(f"Can't get user info from url {error}")
     return person_list
 
 def return_parsed_data():
